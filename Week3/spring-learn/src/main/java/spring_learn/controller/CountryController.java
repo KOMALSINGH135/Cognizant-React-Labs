87889178f9c5ea3f56be3009ad8693e447bb8411
KOMALSINGH135 <komalsingh135.singh@gmail.com>
2026-07-18 02:27:53 +0530

    Add Week3 country controller

---
+++ Week3/spring-learn/src/main/java/spring_learn/controller/CountryController.java
@@ -0,0 +1,23 @@
+package spring_learn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring_learn.Country;
+import spring_learn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        return countryService.getCountry(code);
+
+    }
+}
